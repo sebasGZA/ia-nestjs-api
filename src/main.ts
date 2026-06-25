a290c62529ca96e068795aed5f7fd9f362e8cca3
sebasGZA <sebassga34@gmail.com>
2026-06-24 23:06:02 -0500

    chore: remove useless files and add first database migration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const port: number = parseInt(process.env.PORT!, 10)
+  await app.listen(port, () => {
+    Logger.log(`Server is running on port ${port}`, 'main')
+  });
 }
 void bootstrap();
